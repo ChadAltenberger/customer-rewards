f1375d37384b9e83dccb6c77153d6c4a74eb4a1f
Chad Altenberger <chadaltenberger@gmail.com>
2021-05-11 08:33:11 -0400

    Set up basic structure of app and filled out the table with data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { Table } from 'react-bootstrap';
+import { custData } from './custData';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const calculatePoints = amount => {
+		let points = 0;
+		if (amount > 50 && amount < 100) {
+			points = amount - 50;
+			return points;
+		} else if (amount > 100) {
+			points = (amount - 100) * 2 + 50;
+			return points;
+		} else return 0;
+	};
+
+	const formatter = new Intl.NumberFormat('en-US', {
+		style: 'currency',
+		currency: 'USD'
+	});
+
+	return (
+		<div className='App'>
+			<Table striped bordered hover>
+				<thead>
+					<tr>
+						<th>Customer</th>
+						<th>Transaction Date</th>
+						<th>Amount</th>
+						<th>Points</th>
+					</tr>
+				</thead>
+				<tbody>
+					{custData.map(customer => {
+						return (
+							<tr key={customer.id}>
+								<td>{customer.name}</td>
+								<td>{customer.date}</td>
+								<td>{formatter.format(customer.amount)}</td>
+								<td>{calculatePoints(customer.amount)}</td>
+							</tr>
+						);
+					})}
+				</tbody>
+			</Table>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/custData.js
@@ -0,0 +1,58 @@
+export const custData = [
+	{
+		id: 0,
+		name: 'Screech',
+		amount: 75,
+		date: '01-12-2021',
+		points: null
+	},
+	{
+		id: 1,
+		name: 'Screech',
+		amount: 115,
+		date: '01-24-2021',
+		points: null
+	},
+	{
+		id: 2,
+		name: 'Screech',
+		amount: 94,
+		date: '02-05-2021',
+		points: null
+	},
+	{
+		id: 3,
+		name: 'Zach',
+		amount: 80,
+		date: '02-06-2021',
+		points: null
+	},
+	{
+		id: 4,
+		name: 'Zach',
+		amount: 225,
+		date: '03-14-2021',
+		points: null
+	},
+	{
+		id: 5,
+		name: 'Slater',
+		amount: 45,
+		date: '01-30-2021',
+		points: null
+	},
+	{
+		id: 6,
+		name: 'Slater',
+		amount: 65,
+		date: '02-20-2021',
+		points: null
+	},
+	{
+		id: 7,
+		name: 'Slater',
+		amount: 120,
+		date: '03-17-2021',
+		points: null
+	}
+];
